7f7f6f561e0ce60401ab33dbb75334cb4cf16227
dbohuslov <dbohuslov@comcast.net>
2021-03-03 07:34:51 -0500

    Pushing to GIT Local

---
+++ mcc-project-auth2/src/test/java/com/bah/mcc/MccProjectAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+/*package com.bah.mcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccProjectAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
