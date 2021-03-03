0819f260b97e7aebcefe92505f0708ed7094152f
dbohuslov <dbohuslov@comcast.net>
2021-03-03 07:37:33 -0500

    test

--- mcc-project-auth2/src/main/java/mcc/project/App.java
+++ mcc-project-auth2/src/main/java/mcc/project/App.java
@@ -1,6 +1,6 @@
 package mcc.project;
 
-//working
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
